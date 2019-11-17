6c082edbf07d363889b5d4e3a7bcd08b4b425dd4
DmitryShibunia <dimgo_gamer@mail.ru>
2019-11-17 21:20:52 +0300

    code review

--- src/main/java/com/dmitryshibunia/service/EmployeeService.java
+++ src/main/java/com/dmitryshibunia/service/EmployeeService.java
@@ -1,49 +1,72 @@
 package com.dmitryshibunia.service;
 
-import com.dmitryshibunia.DAO.EmployeeDAO;
 import com.dmitryshibunia.exception.RecordNotFoundException;
 import com.dmitryshibunia.model.Employee;
+import com.dmitryshibunia.repository.EmployeeRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeDAO;
     private final Logger LOGGER = LogManager.getLogger();
 
     @Autowired
-    public EmployeeService(EmployeeDAO employeeDAO){
-        this.employeeDAO = employeeDAO;
+    public EmployeeService(EmployeeRepository repository){
+        this.employeeDAO = repository;
     }
 
-    public List<Employee> getAllEmployees()
-    {
+    public List<Employee> getAllEmployees() {
         LOGGER.info("Call getAllEmployees() method of EmployeeService");
-        return employeeDAO.getAllEmployees();
+        return employeeDAO.findAll();
     }
 
     public Employee getEmployeeById(Long id) throws RecordNotFoundException {
-        LOGGER.info("Call getEmployeeById() method of EmployeeService");
-        return employeeDAO.getEmployeeById(id);
+        LOGGER.info("Call getEmployeeById() method of EmployeeService" + id);
+        Optional<Employee> employee = employeeDAO.findById(id);
+
+        if(employee.isPresent()) {
+            return employee.get();
+        } else {
+            throw new RecordNotFoundException("No employee record exist for given id");
+        }
     }
 
     public int addEmployee(Employee employee){
-        LOGGER.info("Call addEmployee() method of EmployeeService");
-        return employeeDAO.addEmployee(employee);
+        LOGGER.info("Call addEmployee() method of EmployeeService" + employee);
+        Employee addedEmployee= employeeDAO.save(employee);
+        return addedEmployee.getId().intValue();
     }
 
     public Employee updateEmployee(Employee employee) throws RecordNotFoundException {
-        LOGGER.info("Call updateEmployee() method of EmployeeService");
-        return employeeDAO.updateEmployee(employee);
+        LOGGER.info("Call updateEmployee() method of EmployeeService" + employee);
+        Optional<Employee> existingEmployee = employeeDAO.findById(employee.getId());
+
+        if(existingEmployee.isPresent())
+        {
+            employee.setId(existingEmployee.get().getId());
+            employee = employeeDAO.save(employee);
+            return employee;
+        } else {
+            throw new RecordNotFoundException("No employee record exist for given id");
+        }
     }
 
     public void deleteEmployee(Long id) throws RecordNotFoundException {
-        LOGGER.info("Call deleteEmployee() method of EmployeeService");
-        employeeDAO.deleteEmployee(id);
+        LOGGER.info("Call deleteEmployee() method of EmployeeService" + id);
+        Optional<Employee> employee = employeeDAO.findById(id);
+
+        if(employee.isPresent())
+        {
+            employeeDAO.deleteById(id);
+        } else {
+            throw new RecordNotFoundException("No employee record exist for given id");
+        }
     }
 }

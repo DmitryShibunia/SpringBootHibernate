76ba4e86b1c48894ec495b5f88bfd2a31ce2feab
DmitryShibunia <dimgo_gamer@mail.ru>
2019-11-25 12:42:33 +0300

    Swagger configuration added

--- src/main/java/com/dmitryshibunia/Main.java
+++ src/main/java/com/dmitryshibunia/Main.java
@@ -7,10 +7,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.jms.ConnectionFactory;
 
 @EnableJms
+@EnableSwagger2
 @SpringBootApplication
 public class Main {
 
@@ -20,6 +28,29 @@ public class Main {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         configurer.configure(factory, connectionFactory);
         return factory;
+    }
+
+
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                .basePackage("com.dmitryshibunia.controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("Employee Management REST API")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+
     }
 
     public static void main(String[] args) {
